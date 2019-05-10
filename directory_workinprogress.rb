dbba83bacae58592219c601dabd3296cc5c49e48
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-10 15:47:10 +0100

    working cohort groupings

--- directory_workinprogress.rb
+++ directory_workinprogress.rb
@@ -1,37 +1,34 @@
 def input_students
-	max_length = "Please enter the names of the students".length
-	puts "Please enter the names of the students".center(max_length)
-	puts "To finish, just hit return twice".center(max_length)
-	# create an empty array
+
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+
 	students = []
-	# get the first name
-	name = gets.chomp.center(max_length)
+	name = gets.chomp
 
-	# while the name is not empty, repeate this code 
 	while !name.empty? do 
-		# add the student hash to the array 
-		puts "what is your height?".center(max_length)
-		cohort = gets.chomp.center(max_length)
-
-		students << {name: name, cohort: :november }
-		puts "Now we have #{students.count} students".center(max_length)
-		name = gets.chomp.center(max_length)
+		puts "which cohort are you in?"
+		cohort = gets.chomp
+		if cohort != ""
+			students << {name: name, cohort: cohort.to_sym}
+		else students << {name: name, cohort: :none_selected}
+		end
+		puts "Now we have #{students.count} students"
+		name = gets.chomp
 	end
 		# return the array of students
 	return students
 end
 
 def print_header 
-	puts "The students of Villains Academy".center(max_length)
-	puts "------------".center(max_length)
+	puts "The students of Villains Academy"
+	puts "------------"
 end
 
 def print(students)
-
-	pp students
 	counter = 0
 	while counter < students.count do
-		 puts "#{students[counter][:name]} Height: #{students[counter][:height]} (#{students[counter][:cohort]} cohort)".center(max_length)
+		 puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
 		 counter += 1
 	end
 end
@@ -52,10 +49,35 @@ def print_short_names(students)
 end
 
 def print_footer(students)
-	puts "Overall, we have #{students.count} great students".center(max_length)
+	puts "Overall, we have #{students.count} great students"
+end
+
+def group_by_cohort(students)
+	cohort_groups = {}
+
+
+
+	students.each do |x| 
+		
+		cohorts = x[:cohort]
+		name = x[:name]
+
+		if cohort_groups[cohorts] == nil
+			cohort_groups[cohorts] = []
+		end
+		cohort_groups[cohorts].push(name)
+		
+	end
+	pp cohort_groups
+
+	# pp existing_cohorts
+
+	# cohort_groups = {}
+	# existing_cohorts.map {|x| x => students[x][:name]}
 end
 
 students = input_students
-print_header
-print(students)
-print_footer(students)
+group_by_cohort(students)
+# print_header
+# print(students)
+# print_footer(students)
