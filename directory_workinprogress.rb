46e913ee013860d9ecd8f49a3c2eaa33d20c22b7
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-10 14:24:32 +0100

    centered and added height

--- directory_workinprogress.rb
+++ directory_workinprogress.rb
@@ -1,36 +1,37 @@
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	max_length = "Please enter the names of the students".length
+	puts "Please enter the names of the students".center(max_length)
+	puts "To finish, just hit return twice".center(max_length)
 	# create an empty array
 	students = []
 	# get the first name
-	name = gets.chomp
+	name = gets.chomp.center(max_length)
+
 	# while the name is not empty, repeate this code 
 	while !name.empty? do 
 		# add the student hash to the array 
-		students << {name: name, cohort: :november}
-		puts "Now we have #{students.count} students"
-		name = gets.chomp
+		puts "what is your height?".center(max_length)
+		cohort = gets.chomp.center(max_length)
+
+		students << {name: name, cohort: :november }
+		puts "Now we have #{students.count} students".center(max_length)
+		name = gets.chomp.center(max_length)
 	end
 		# return the array of students
 	return students
 end
 
 def print_header 
-	puts "The students of Villains Academy"
-	puts "------------"
+	puts "The students of Villains Academy".center(max_length)
+	puts "------------".center(max_length)
 end
 
-# def print(students)
-# 	students.each do |student|		
-# 		puts "#{students[:name]} (#{students[:cohort]} cohort)"
-# 	end
-# end
-
 def print(students)
+
+	pp students
 	counter = 0
 	while counter < students.count do
-		 puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+		 puts "#{students[counter][:name]} Height: #{students[counter][:height]} (#{students[counter][:cohort]} cohort)".center(max_length)
 		 counter += 1
 	end
 end
@@ -51,7 +52,7 @@ def print_short_names(students)
 end
 
 def print_footer(students)
-	puts "Overall, we have #{students.count} great students"
+	puts "Overall, we have #{students.count} great students".center(max_length)
 end
 
 students = input_students
